a660ceaf6fe1134d77cffa34f21f7dab92dc205f
Nicoll <U201918378@upc.edu.pe>
2024-06-10 22:37:35 -0500

    Se eliminó seguridad

--- src/main/java/com/example/finanzasgrupo5backend/Users/Service/UserServiceImpl.java
+++ src/main/java/com/example/finanzasgrupo5backend/Users/Service/UserServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class UserServiceImpl implements IUserService {
-    @Autowired
+
     IUserRepository userRepository;
     @Override
     public void insertUser(Users users) {
